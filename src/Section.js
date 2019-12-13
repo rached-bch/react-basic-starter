78adde6b93d40ddc8a6cbf65d77687576212dee4
Rached <bch.rached@gmail.com>
2019-12-13 15:03:05 +0100

    Utilisation de prop types dans les components

--- src/Section.js
+++ src/Section.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Section extends Component {
+  static defaultProps = {
+    text: "Texte temporaire par défaut"
+  };
+
+  static propTypes = {
+    text: PropTypes.string.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.textInput = props.text;
+  }
   render() {
-    const text = this.props.text;
-    return <div>{text}</div>;
+    return <div>{this.textInput}</div>;
   }
 }
