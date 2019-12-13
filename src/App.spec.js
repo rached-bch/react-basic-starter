864252ce313ce696f2f4868d7c2794a6938b2f66
Rached <bch.rached@gmail.com>
2019-12-13 17:05:17 +0100

    Test en utilisant chai enzyme et sinon pour simuler avoir les fcts spy et ne pas perdre les conditions chai

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,14 +1,29 @@
 import { expect } from "chai";
 import React from "react";
 import { shallow } from "enzyme";
+import sinon from "sinon";
 
 import App from "./App";
 import GuessCount from "./GuessCount";
+import Section from "./Section";
+import Card from "./Card";
 
 describe("<App />", () => {
   it("renders without crashing", () => {
     const wrapper = shallow(<App />);
 
     expect(wrapper).to.contain(<GuessCount guesses={0} />);
+  });
+});
+
+describe("<Card/>", () => {
+  it("Card should trigger its `onClick` prop when clicked", () => {
+    const onClick = sinon.spy();
+    const wrapper = shallow(
+      <Card card="😀" feedback="hidden" onClick={onClick} />
+    );
+
+    wrapper.simulate("click");
+    expect(onClick).to.have.been.callCount(1);
   });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -10,10 +10,12 @@ import { configure as configureEnzyme } from "enzyme";
 import createChaiEnzyme from "chai-enzyme";
 import createChaiJestDiff from "chai-jest-diff";
 import dirtyChai from "dirty-chai";
+import sinonChai from "sinon-chai";
 
 chai
   .use(dirtyChai)
   .use(createChaiJestDiff())
-  .use(createChaiEnzyme());
+  .use(createChaiEnzyme())
+  .use(sinonChai);
 
 configureEnzyme({ adapter: new Adapter() });
