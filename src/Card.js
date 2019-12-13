3f379cd788d768b3ee154cd650c0ef58235dbd8b
Rached <bch.rached@gmail.com>
2019-12-13 14:54:08 +0100

    Utilisation de prop types

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Card = ({ card, feedback, onClick }) => {
   return (
-    <div className="card" onClick={onClick === false ? () => {} : onClick()}>
+    <div
+      className="card"
+      onClick={onClick === undefined ? () => {} : onClick()}
+    >
       H
     </div>
   );
 };
+
+Card.propTypes = {
+  car: PropTypes.string.isRequired,
+  feedback: PropTypes.oneOf([
+    "hidden",
+    "visible",
+    "justMatched",
+    "justMismatched"
+  ]).isRequired,
+  onClick: PropTypes.func
+};
+
 Card.defaultProps = {
   car: "🐱",
-  feedback: "visible",
-  onClick: false
+  feedback: "visible"
 };
 
 export default Card;
