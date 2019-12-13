e3948dc1c0c3dde55eb598c1017e8d4ea8764a0a
Rached <bch.rached@gmail.com>
2019-12-13 16:00:13 +0100

    Exemple rendu dynamique avec state clear interval mounted comonent et

--- src/TimeCounter.js
+++ src/TimeCounter.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 export default class TimeCounter extends Component {
-  state = {
-    currentSecond: 0
-  };
   constructor(props) {
     super(props);
+    this.state = {
+      currentSecond: 0,
+      executedTimer: false
+    };
+  }
+  componentDidMount() {
     this.simulateStatTimer();
   }
   simulateStatTimer = () => {
-    this.executedTimer = setInterval(() => {
-      console.log("interval executed");
-      const seconds = this.state.currentSecond + 1;
-      this.setState({ currentSecond: seconds });
-    }, 1000);
+    // let executedTimer = setInterval(() => {
+    //   console.log("interval executed");
+    //   const seconds = this.state.currentSecond + 1;
+    //   this.setState({ currentSecond: seconds });
+    // }, 1000);
+    this.setState(prevState => ({
+      executedTimer: setInterval(() => {
+        console.log("interval executed");
+        const seconds = this.state.currentSecond + 1;
+        this.setState({ currentSecond: seconds });
+      }, 1000)
+    }));
   };
   simulateStopTimer = () => {
-    clearInterval(this.executedTimer);
+    clearInterval(this.state.executedTimer);
+    this.setState(prevState => ({
+      executedTimer: false
+    }));
   };
   render() {
     return (
